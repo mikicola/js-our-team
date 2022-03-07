15e485bf38821c747c92e74aaae00586d59edfc4
mikicola <michela.colangelo@gmail.com>
2022-03-07 16:33:21 +0100

    creo funzione

--- script.js
+++ script.js
@@ -5,26 +5,57 @@ Prendendo come riferimento la card di esempio presente nell'html, stampiamo dina
 
 const arrTeam = [
     {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'img/angela-caroll-chief-editor.jpg',
+    },
+    {
         name: 'Angela Caroll',
         job: 'Chief Editor',
+        img: 'img/angela-caroll-chief-editor.jpg',
     },
     {
         name: 'Walter Gordon',
         job: 'Office Manager',
+        img: 'img/angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Angela Lopez',
         job: 'Social Media Manager',
+        img: 'img/angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Scot Estrada',
         job: 'Developer',
+        img: 'img/angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Barbara Ramos',
         job: 'Graphic Designer',
+        img: 'img/angela-caroll-chief-editor.jpg'
     },
 ]
 console.log(arrTeam[0].name)
 // oppure
 //console.log(arrTeam[0]['name'])
+
+
+const teamContainer = document.querySelector('.team-container')
+function createCard(){
+    let codeCard =
+    `
+    <div class="team-card">
+        <div class="card-image">
+            <img
+            src=${img}
+            alt=${name}
+            />
+        </div>
+        <div class="card-text">
+            <h3>${name}</h3>
+            <p>${job}</p>
+        </div>
+    </div>
+    `
+    teamContainer.innerHTML = teamContainer.innerHTML + codeCard;
+}
